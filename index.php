46f3fc4d21787888a7c3a46bf94c7227b7ec6651
alvinkohcm <alvin@sigma2k.com>
2018-05-24 15:46:30 +0800

    Update index.php with reseller.enom.com domain
    Added https to request

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 /******************************************************************************
 * ENOM SETTINGS
 ******************************************************************************/
-$settings[api] = "http://dynamic.name-services.com/interface.asp";
+$settings[api] = "https://reseller.enom.com/interface.asp";
 
 /******************************************************************************
 * START
@@ -34,6 +34,7 @@ $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
 curl_setopt($ch, CURLOPT_HEADER, 0);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
 $data = curl_exec($ch);
 curl_close($ch);
 
